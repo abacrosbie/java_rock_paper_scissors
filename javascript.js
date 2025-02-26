c03cb5ea703b7ae10c8646984ec736d2f01c053c
Addison Crosbie <abacrosbie@gmail.com>
2025-02-26 14:52:24 +0000

    rearranged variables for computer choice and human choice

--- javascript.js
+++ javascript.js
@@ -1,25 +1,27 @@
 function getComputerChoice(){
     const minCeiled = Math.ceil(1); // Sets minimum
     const maxFloored = Math.floor(3); // Sets maximum
-    let computerChoice = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); //Randomises choice   
-    
-    if (computerChoice === 1){ // Converts random number to 'Rock, Paper or Scissors'
-        return "Rock";
+    let computerChoiceValue = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); //Randomises choice   
+    let computerChoice = "";
+
+    if (computerChoiceValue === 1){ // Converts random number to 'Rock, Paper or Scissors'
+        computerChoice = "Rock";
     }
-    else if (computerChoice === 2){
-        return "Paper";
+    else if (computerChoiceValue === 2){
+        computerChoice = "Paper";
     }
-    else if (computerChoice === 3){
-        return "Scissors";
+    else if (computerChoiceValue === 3){
+        computerChoice = "Scissors";
     }
 }
-console.log(getComputerChoice()); // Show computer choice
 
 function getHumanChoice(humanChoice){ // Sets human choice
     humanChoice = prompt("Rock, Paper or Scissors?"); // Prompts user to enter choice
-    return humanChoice;
 }
-console.log(getHumanChoice()); // Show Human choice
 
 let humanScore = 0 // Sets scores
-let computerScore = 0
+let computerScore = 0 
+
+function playRound(computerChoice,humanChoice){
+    
+}
