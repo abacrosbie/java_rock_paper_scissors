7640bc4683430d36cdaea0ae81d2868dbd285e2b
Addison Crosbie <abacrosbie@gmail.com>
2025-02-26 14:21:29 +0000

    added conversion to rock paper or scissors to javascript.js

--- javascript.js
+++ javascript.js
@@ -3,8 +3,15 @@ function getComputerChoice(){
     const maxFloored = Math.floor(3); // Sets maximum
     let computerChoice = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); //Randomises choice   
     
-    return computerChoice
-    
+    if (computerChoice === 1){ // Converts random number to 'Rock, Paper or Scissors'
+        return "Rock";
+    }
+    else if (computerChoice === 2){
+        return "Paper";
+    }
+    else if (computerChoice === 3){
+        return "Scissors";
+    }
 }
 
 console.log(getComputerChoice());
