6e67139f6b942509b6507bea391556775a623d25
Addison Crosbie <abacrosbie@gmail.com>
2025-02-26 16:35:01 +0000

    added playround and simple version of results

--- javascript.js
+++ javascript.js
@@ -1,27 +1,40 @@
 function getComputerChoice(){
     const minCeiled = Math.ceil(1); // Sets minimum
     const maxFloored = Math.floor(3); // Sets maximum
-    let computerChoiceValue = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); //Randomises choice   
-    let computerChoice = "";
-
-    if (computerChoiceValue === 1){ // Converts random number to 'Rock, Paper or Scissors'
+    let computerValueChoice = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); //Randomises choice   
+    
+    if (computerValueChoice === 1){ // Converts random number to 'Rock, Paper or Scissors'
         computerChoice = "Rock";
     }
-    else if (computerChoiceValue === 2){
+    else if (computerValueChoice === 2){
         computerChoice = "Paper";
     }
-    else if (computerChoiceValue === 3){
+    else if (computerValueChoice === 3){
         computerChoice = "Scissors";
     }
 }
 
-function getHumanChoice(humanChoice){ // Sets human choice
+function getHumanChoice(){ // Sets human choice
     humanChoice = prompt("Rock, Paper or Scissors?"); // Prompts user to enter choice
 }
 
 let humanScore = 0 // Sets scores
 let computerScore = 0 
 
-function playRound(computerChoice,humanChoice){
-    
+function playRound(humanChoice, computerChoice){
+   if (humanChoice === computerChoice){
+    return "lol draw";
+   }
+   else {return "Nah mate";}
 }
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanChoice)
+console.log(computerChoice)
+console.log(playRound(humanChoice,computerChoice))
