f417154c2dd0a014b818a73bdb8dfbee71072237
Addison Crosbie <abacrosbie@gmail.com>
2025-03-01 12:43:36 +0000

    branched off to explore playGame function

--- javascript.js
+++ javascript.js
@@ -43,14 +43,8 @@ function playRound(humanChoice, computerChoice){
    }
 }
 
-
-
-
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-
-//playRound(humanSelection, computerSelection);
 
 //console.log(humanChoice)
 //console.log(computerChoice)
